4166e11e41765385c1d6f93fb0bae2d159e4e7b0
“Nikita <nikitaberezhnov27@gmail.com>
2022-05-13 18:42:54 +0300

    fix: поправил форму обратной связи

--- src/app/footer/feedback/feedback.component.ts
+++ src/app/footer/feedback/feedback.component.ts
@@ -12,15 +12,7 @@ export class FeedbackComponent implements OnInit {
     this.feedback = this.fb.group({
       userName: ['', [Validators.required, Validators.pattern(/[А-я]/)]],
       userId: ['', [Validators.required]],
-      text: [
-        '',
-        [
-          Validators.minLength(10),
-          Validators.pattern(
-            '^[a-zA-Z0-9а-яёА-ЯЁ]*[-_— .@]?[a-zA-Z0-9а-яёА-ЯЁ]*.?[a-zA-Z0-9а-яёА-ЯЁ]*$'
-          ),
-        ],
-      ],
+      text: ['', [Validators.minLength(10)]],
     });
   }
   ngOnInit(): void {}
