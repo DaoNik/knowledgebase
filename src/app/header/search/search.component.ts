7d54e46b0200553a43c19f1fa65b7d1c3edf048a
DaoNik <nikitataranin@gmail.com>
2022-05-16 19:58:29 +0300

    fix route

--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -6,13 +6,13 @@ import { IArticle } from 'src/app/interfaces/article';
 import { SearchService } from './search.service';
 
 interface IFilter {
-  title: string,
-  status: boolean
+  title: string;
+  status: boolean;
 }
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
   searchQuery = new FormControl();
@@ -20,64 +20,75 @@ export class SearchComponent implements OnInit {
   filterOptions: IFilter[] = [
     {
       title: 'Логистика',
-      status: true
+      status: true,
     },
     {
       title: 'Серверная сторона',
-      status: true
+      status: true,
     },
     {
       title: 'Базы данных',
-      status: true
+      status: true,
     },
     {
       title: 'Клиентская сторона',
-      status: true
+      status: true,
     },
     {
       title: 'Склад',
-      status: true
+      status: true,
     },
     {
       title: 'Пункты выдачи',
-      status: true
-    }
-  ]
+      status: true,
+    },
+  ];
   filteredResults!: Observable<IArticle[]>;
   foundArticles!: IArticle[];
 
-  constructor(
-    private searchService: SearchService, 
-    private router: Router
-  ) { }
+  constructor(private searchService: SearchService, private router: Router) {}
 
   ngOnInit(): void {
     this.filteredResults = this.searchQuery.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value)),
+      map((value) => this._filter(value))
     );
-    this.searchService.getArticles()
-    .subscribe((articles: IArticle[]) => this.foundArticles = articles)
+    this.searchService
+      .getArticles()
+      .subscribe((articles: IArticle[]) => (this.foundArticles = articles));
   }
 
   private _filter(value: string): IArticle[] {
     const filterValue = value.toLowerCase();
-    const filterTags = this.filterOptions.filter(item => { return item.status })
-                                  .map(item => { return item.title });
+    const filterTags = this.filterOptions
+      .filter((item) => {
+        return item.status;
+      })
+      .map((item) => {
+        return item.title;
+      });
 
-    return this.foundArticles ? 
-           this.foundArticles.filter(result => result.title.toLowerCase().includes(filterValue) && 
-                                               filterTags.includes(result.category)) :
-           this.foundArticles
+    return this.foundArticles
+      ? this.foundArticles.filter(
+          (result) =>
+            result.title.toLowerCase().includes(filterValue) &&
+            filterTags.includes(result.category)
+        )
+      : this.foundArticles;
   }
 
   search() {
-    const filterTags = this.filterOptions.filter(item => { return item.status })
-                                  .map(item => { return item.title });
-    this.searchService.goToSearchResults(this.searchQuery.value, filterTags)
+    const filterTags = this.filterOptions
+      .filter((item) => {
+        return item.status;
+      })
+      .map((item) => {
+        return item.title;
+      });
+    this.searchService.goToSearchResults(this.searchQuery.value, filterTags);
   }
 
   goToArticle(article: IArticle) {
-      this.router.navigate(['article', `${article.id}`]);
+    this.router.navigate(['article', `${article.id}`]);
   }
 }
--- src/app/header/search/search.service.ts
+++ src/app/header/search/search.service.ts
@@ -1,26 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { IArticle } from 'src/app/interfaces/article';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SearchService {
-  private url: string = 'http://localhost:4500/mock-articles.json'
+  // private url: string = 'http://localhost:4500/mock-articles.json'
 
   constructor(
-    private http: HttpClient, 
-    private router: Router
-  ) { }
+    private http: HttpClient,
+    private router: Router,
+    @Inject('API_URL') private apiUrl: string
+  ) {}
 
   getArticles(): Observable<IArticle[]> {
-    return this.http.get<IArticle[]>(this.url)
+    return this.http.get<IArticle[]>(this.apiUrl);
   }
 
   goToSearchResults(title: string, categories: string[]) {
-    if (title == null) this.router.navigate(['search-result', ``, `${categories}`]);
+    if (title == null)
+      this.router.navigate(['search-result', ``, `${categories}`]);
     else this.router.navigate(['search-result', `${title}`, `${categories}`]);
   }
 }
