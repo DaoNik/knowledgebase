a50f9e8b9b13bd78908ee4836c612a8ad0754a23
Alexander <warrior0edem@gmail.com>
2022-05-16 12:41:54 +0300

    added route for articles

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ArticleService } from '../article/article.service';
 import { IArticle } from '../interfaces/article';
 
@@ -10,11 +11,18 @@ import { IArticle } from '../interfaces/article';
 export class MainComponent implements OnInit {
   articles: IArticle[] = [];
 
-  constructor(private articleService: ArticleService) {}
+  constructor(
+    private articleService: ArticleService, 
+    private router: Router
+    ) {}
 
   ngOnInit(): void {
     this.articleService
       .getArticles()
       .subscribe((articles) => (this.articles = articles));
+  }
+
+  openArticle(i: number) {
+    this.router.navigate(['article', `${i}`]);
   }
 }
