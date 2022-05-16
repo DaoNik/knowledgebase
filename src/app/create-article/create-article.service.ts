af2f993207945146b82a58ee9a5258f57e483238
byteCD <dima.s322@yandex.ru>
2022-05-16 23:26:31 +0600

    Update create article

--- src/app/create-article/create-article.service.ts
+++ src/app/create-article/create-article.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IArticle } from './../interfaces/article';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CreateArticleService {
+  constructor(
+    @Inject('API_URL') private apiUrl: string,
+    private http: HttpClient
+  ) {}
 
-  constructor() { }
+  getArticle(id: string): Observable<IArticle> {
+    return this.http.get<IArticle>(`${this.apiUrl}/${id}`);
+  }
+
+  createArticle(article: IArticle): Observable<IArticle> {
+    return this.http.post<IArticle>(`${this.apiUrl}`, article);
+  }
+
+  editArticle(id: string, article: IArticle): Observable<IArticle> {
+    return this.http.patch<IArticle>(`${this.apiUrl}/${id}`, article);
+  }
 }
