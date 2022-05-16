0a0b8fe71df6d3c2afd663a5806f7be2252e70a0
byteCD <dima.s322@yandex.ru>
2022-05-16 18:16:48 +0600

    Fix errors

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -6,38 +6,38 @@ import { ArticleService } from './article.service';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
-  articles: IArticle[] = [{
-    id: 1,
-    title: "Как кинуть Карину в мут?",
-    category: "Мут",
-    description: "Берешь и перетаскиваешь в мут, что еще думать то?!",
-    dateCreate: "1652442180701",
-    dateUpdate: "1652442180701",
-    authors: ["Yaunberzinsh Alexander", "Taranin Nikita"],
-    respondents: ["Taranin Nikita", "Berezhnov Nikita"],
-    content: {
-      text: "На сервере в дискорде во время голосового звонка на компьютере можно перетащить мышкой Карину в голосовой канал мут.",
-      image: ""
+  articles: IArticle[] = [
+    {
+      id: 1,
+      title: 'Как кинуть Карину в мут?',
+      category: 'Мут',
+      description: 'Берешь и перетаскиваешь в мут, что еще думать то?!',
+      dateCreate: '1652442180701',
+      dateUpdate: '1652442180701',
+      authors: ['Yaunberzinsh Alexander', 'Taranin Nikita'],
+      respondents: ['Taranin Nikita', 'Berezhnov Nikita'],
+      content:
+        'На сервере в дискорде во время голосового звонка на компьютере можно перетащить мышкой Карину в голосовой канал мут.',
+      tags: [],
     },
-    tags: []
-  }];
+  ];
 
   articleId!: number;
 
   constructor(
     private articlesServ: ArticleService,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.articlesServ.getArticles()
-    .subscribe((articles: IArticle[]) => this.articles = articles)
+    this.articlesServ
+      .getArticles()
+      .subscribe((articles: IArticle[]) => (this.articles = articles));
     this.route.params.subscribe((params: Params) => {
-      this.articleId = Number(params["id"]);
-    })
+      this.articleId = Number(params['id']);
+    });
   }
-
 }
