9d15c535f975c372c80409740351d4385b4d8ef5
DaoNik <nikitataranin@gmail.com>
2022-05-16 20:05:03 +0300

    fix method

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -6,28 +6,27 @@ import { IArticle } from '../interfaces/article';
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
   articles: IArticle[] = [];
 
-  constructor (
-    private articleService: ArticleService, 
+  constructor(
+    private articleService: ArticleService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.articleService
-        .getArticles()
-        .subscribe((articles) => {
-          this.articles = articles.filter(item => {
-            if (item.title.toLowerCase().includes(params['title'].toLowerCase()) && params['categories'].includes(item.category)) return true;
-            else return false;
-          })
-        });
-    })
+    this.route.params.subscribe((params) => {
+      this.articleService.getArticles().subscribe((articles) => {
+        this.articles = articles.filter(
+          (item) =>
+            item.title.toLowerCase().includes(params['title'].toLowerCase()) &&
+            params['categories'].includes(item.category)
+        );
+      });
+    });
   }
 
   openArticle(id: number) {
