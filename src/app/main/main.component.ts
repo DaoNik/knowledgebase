74711d584dfa6344317b4d393fe0bb8ecebfaf8d
kkoroleva <korolevakarinka22@gmail.com>
2022-05-19 16:09:26 +0300

    add pagination for main page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,16 +10,37 @@ import { IArticle } from '../interfaces/article';
 })
 export class MainComponent implements OnInit {
   articles: IArticle[] = [];
+  articlesOnPage: number = 8;
+  currentPageArticles: IArticle[] = this.articles.slice(0, this.articlesOnPage);
+  currentPage: number = 1;
+  pages: number[] = [];
 
   constructor(private articleService: ArticleService, private router: Router) {}
 
   ngOnInit(): void {
     this.articleService
       .getArticles()
-      .subscribe((articles) => (this.articles = articles));
+      .subscribe((articles) => {
+        this.articles = articles;
+        this.currentPageArticles = this.articles.slice(0, this.articlesOnPage);
+        this.countPages();
+      });
   }
 
   openArticle(id: string) {
     this.router.navigateByUrl(`article/${id}`);
+  }
+
+  countPages(): void {
+    this.pages = [];
+    for (let i = 0; i < (this.articles.length / this.articlesOnPage); i++) {
+      this.pages.push(i + 1);
+    }
+  }
+
+  pageClick(page: number) {
+    this.currentPage = page;
+    const startArticle: number = (page - 1) * this.articlesOnPage;
+    this.currentPageArticles = this.articles.slice(startArticle, startArticle + this.articlesOnPage);
   }
 }
