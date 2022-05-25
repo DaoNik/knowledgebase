f741884091741fe195254610a53c63442ab27f51
Alexander <warrior0edem@gmail.com>
2022-05-25 14:50:13 +0300

    созданы методы создания колонок и тасок

--- src/app/tasks-manager/interfaces/taskList.interface.ts
+++ src/app/tasks-manager/interfaces/taskList.interface.ts
@@ -1,4 +1,41 @@
-export interface ITasksList {
+export interface ITasksList2 {
     name: string;
     tasks: string[];
+}
+
+export interface ITask {
+    authors: [];
+    category: string;
+    columnId: number;
+    createdAt: string;
+    description: string;
+    id: number;
+    priority: any;
+    respondents: [];
+    status: any;
+    tags: [],
+    title: string;
+    updatedAt: string;
+}
+
+export interface IBoard {
+    authors: string[];
+    category: string[];
+    columns: IColumn[];
+    createdAt: string;
+    description: string;
+    id: number;
+    respondents: string[];
+    tags: string[],
+    title: string;
+    updatedAt: string;
+}
+
+export interface IColumn {
+    boardId: number;
+    createdAt: string;
+    id: number;
+    title: string;
+    updatedAt: string;
+    tasks?: any[];
 }
