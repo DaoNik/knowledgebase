da9e2f10019b3e8a1b190b8b3396d244ca1205a0
Leonid (notebook) <leonidrawhide@gmail.com>
2022-05-26 22:45:54 +0300

    fixed conflicts

--- src/app/tasks-manager/modal-task/modal-task.component.ts
+++ src/app/tasks-manager/modal-task/modal-task.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ITypeOption } from './modal-task-interface';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { map, Observable, startWith } from 'rxjs';
