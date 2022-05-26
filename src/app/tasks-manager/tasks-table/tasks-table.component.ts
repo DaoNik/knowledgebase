1d5ae9c296eb187342aab6e20eebd326e6b9f3fd
максим Звягин <maksbossgd@inbox.ru>
2022-05-26 15:32:09 +0300

    попытка починить таблу

--- src/app/tasks-manager/tasks-table/tasks-table.component.ts
+++ src/app/tasks-manager/tasks-table/tasks-table.component.ts
@@ -1,15 +1,22 @@
-import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnInit,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { map, Observable, pipe } from 'rxjs';
 import { ITask } from '../interfaces/taskList.interface';
 import { TasksManagerService } from '../tasks-manager.service';
 
-export interface IUserData {
-  id: string;
-  name: string;
-  progress: string;
-  assigne: string;
+export interface ITableTasks {
+  id: number;
+  title: string;
+  status: string;
+  respondents: string[];
   priority: string;
 }
 
@@ -18,56 +25,51 @@ export interface IUserData {
   templateUrl: './tasks-table.component.html',
   styleUrls: ['./tasks-table.component.scss'],
 })
-export class TasksTableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'assigne', 'priority'];
+export class TasksTableComponent
+  implements OnInit, AfterViewInit
+{
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'status',
+    'respondents',
+    'priority',
+  ];
 
-  // articles: IUserData[] = [
-  //   {
-  //     id: '1',
-  //     name: 'Maia',
-  //     progress: 'Done',
-  //     assigne: 'Maia',
-  //     priority: 'Medium'
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Asher',
-  //     progress: 'in Progress',
-  //     assigne: 'Asher',
-  //     priority: 'Low'
-  //   },
-  //   {
-  //     id: '30',
-  //     name: 'Atticus',
-  //     progress: 'To Do',
-  //     assigne: 'Atticus',
-  //     priority: 'Medium'
-  //   },
-  //   {
-  //     id: '4',
-  //     name: 'Olivia',
-  //     progress: 'Done',
-  //     assigne: 'Olivia',
-  //     priority: 'Low'
-  //   },
-  //   {
-  //     id: '5',
-  //     name: 'Olivia',
-  //     progress: 'in Progress',
-  //     assigne: 'Olivia',
-  //     priority: 'High'
-  //   },
-  // ]
+  tasks$ = this.taskServ.getTasks();
+  filterTasks: ITableTasks[] = [];
 
-  dataSource!: MatTableDataSource<ITask>;
+  dataSource!: MatTableDataSource<ITableTasks>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  constructor(private elRef: ElementRef, private taskServ: TasksManagerService) {
-    // this.dataSource = new MatTableDataSource(this.articles);
-    this.taskServ.getTasks().subscribe(tasks => this.dataSource = new MatTableDataSource(tasks));
+  constructor(
+    private elRef: ElementRef,
+    private taskServ: TasksManagerService
+  ) {}
+
+  ngOnInit(): void {
+    this.tasks$.pipe(
+      map((tasks) => {
+        tasks.map((task) => {
+          this.filterTasks.push(
+           {
+              id: task.id, 
+              title: task.title, 
+              status: task.status, 
+              respondents: task.respondents,
+              priority: task.priority
+            }
+          )
+        })
+      })
+    ).subscribe(() => {
+      this.dataSource = new MatTableDataSource(this.filterTasks.sort());
+
+    })
+
   }
 
   ngAfterViewInit() {
@@ -75,18 +77,17 @@ export class TasksTableComponent implements AfterViewInit {
       '.mat-paginator-page-size-label'
     ).textContent = 'Отобразить: ';
 
-    console.log(this.dataSource.sort)
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
-
 }
