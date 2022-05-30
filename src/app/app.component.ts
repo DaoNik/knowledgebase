b3300c7fa8fb1fee2f1892542eed1b0e4b3e35e0
byteCD <dima.s322@yandex.ru>
2022-05-30 21:40:13 +0600

    Add socket client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'База знаний WB';
+  socket = io('http://localhost:3000', {
+    path: '/api/socket',
+  });
+
+  ngOnInit(): void {
+    this.socket.emit(
+      'createComment',
+      { text: '12345', author: 'Author', taskId: 1 },
+      (data: any) => console.log(data)
+    );
+  }
 }
