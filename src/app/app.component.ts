7834af7a9f53518b772f1dbd03b77ea644b34a8c
максим Звягин <maksbossgd@inbox.ru>
2022-05-30 22:05:56 +0300

    add: Анимация отображения сообщения об ошибке

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { ErrorModalService } from './error-modal/error-modal.service';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
+  animations: [
+    trigger(
+      'errorAnimation', [
+        transition(':enter', [
+          style({transform: 'opacity(0)', opacity: 0}),
+          animate('300ms', style({transform: 'opacity(1)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'opacity(1)', opacity: 1}),
+          animate('300ms', style({transform: 'opacity(0)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
