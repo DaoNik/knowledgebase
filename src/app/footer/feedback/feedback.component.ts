d9ce2d8e76136b826af7c05e1ec7630d6686a135
“Nikita <nikitaberezhnov27@gmail.com>
2022-05-30 17:04:25 +0300

    fix

--- src/app/footer/feedback/feedback.component.ts
+++ src/app/footer/feedback/feedback.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,14 +8,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss'],
 })
-export class FeedbackComponent implements OnInit {
+export class FeedbackComponent {
   feedback: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    @Inject('API_URL') private apiUrl: string
+  ) {
     this.feedback = this.fb.group({
       userName: ['', [Validators.required, Validators.pattern(/[А-я]/)]],
       userId: ['', [Validators.required]],
       text: ['', [Validators.minLength(10)]],
     });
   }
-  ngOnInit(): void {}
+
+
 }
