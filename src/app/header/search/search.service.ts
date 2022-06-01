626702f29b3d5b912acdddca146fabc111d7c9a6
максим Звягин <maksbossgd@inbox.ru>
2022-06-01 15:38:08 +0300

    add: визуальное отображение ошибок

--- src/app/header/search/search.service.ts
+++ src/app/header/search/search.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, throwError } from 'rxjs';
+import { ErrorModalService } from 'src/app/error-modal/error-modal.service';
 import { IArticle } from 'src/app/interfaces/article';
 
 @Injectable({
@@ -13,12 +14,16 @@ export class SearchService {
   constructor(
     private http: HttpClient,
     private router: Router,
-    @Inject('API_URL') private apiUrl: string
+    @Inject('API_URL') private apiUrl: string,
+    private errorService: ErrorModalService
   ) {}
 
   getArticles(): Observable<IArticle[]> {
     return this.http.get<IArticle[]>(this.apiUrl).pipe(
       catchError((error: HttpErrorResponse) => {
+        this.errorService.visibleForError(
+          error.error.message[error.error.message.length - 1]
+        );
         return throwError(() => error);
       })
     );
