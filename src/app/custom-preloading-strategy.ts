fbdbe626cf0c68e4b2a74380d78ebaa4a4a5ead6
максим Звягин <maksbossgd@inbox.ru>
2022-06-06 22:15:06 +0300

    clear logs in custom preloading

--- src/app/custom-preloading-strategy.ts
+++ src/app/custom-preloading-strategy.ts
@@ -10,15 +10,12 @@ export class CustomPreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, loadMe: () => Observable<any>): Observable<any> {
     if (route.data && route.data['preload']) {
       var delay: number = route.data['delay'];
-      console.log('preload called on ' + route.path + ' delay is ' + delay);
       return timer(delay).pipe(
         mergeMap((_) => {
-          console.log('Loading now ' + route.path);
           return loadMe();
         })
       );
     } else {
-      console.log('no preload for the path ' + route.path);
       return of(null);
     }
   }
